a575f63d74946c95b9c29d14afc39bfb86f1e3d3
swarminglogic <github@swarminglogic.com>
2013-09-15 12:44:21 +0200

    Fixed missing OpenGL 3+ functions
    Fixed missing OpenGL 3+ functions
        src/ui/SDL_opengl.h

--- src/ui/SDL_opengl.h
+++ src/ui/SDL_opengl.h
@@ -1,7 +1,19 @@
 #ifndef UI_SDL_OPENGL_H
 #define UI_SDL_OPENGL_H
 
+// Required definition call for OpenGL 3.0+ functions
+#define GL_GLEXT_PROTOTYPES 1
+
+// SDL comes with built in glext.h, uncomment to use local one
+#define NO_SDL_GLEXT
+
+
+// Regular includs
 #include <SDL2/SDL_opengl.h>
+
+#ifdef NO_SDL_GLEXT
+#include <GL/glext.h>
+#endif
 
 
 #endif
