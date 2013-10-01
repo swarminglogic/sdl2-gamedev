355fb4f6abeb451d32dad8f90425249de643c2d4
swarminglogic <github@swarminglogic.com>
2013-10-01 09:55:07 +0200

    Templetized functions in GlUtil
    Templetized functions in GlUtil
        src/ui/GlUtil.cpp
        src/ui/GlUtil.h

--- src/ui/GlUtil.cpp
+++ src/ui/GlUtil.cpp
@@ -3,20 +3,6 @@
 #include <cassert>
 
 
-GLuint GlUtil::prepareVertexBuffer(const std::vector<GLfloat>& data,
-                                   GLenum target,
-                                   GLenum usage)
-{
-  GLuint buffername = 0;
-  glGenBuffers(1, &buffername);
-  glBindBuffer(target, buffername);
-  glBufferData(GL_ARRAY_BUFFER,
-               data.size() * sizeof(data[0]),
-               &data[0],
-               usage);
-  return buffername;
-}
-
 GLuint GlUtil::prepareVertexBuffer(GLsizeiptr size,
                                    GLenum target,
                                    GLenum usage)
@@ -28,13 +14,3 @@ GLuint GlUtil::prepareVertexBuffer(GLsizeiptr size,
   return buffername;
 }
 
-
-void GlUtil::fillVertexBuffer(const std::vector<GLfloat>& data,
-                              GLenum target)
-{
-  assert(!data.empty() && "Cannot fill vertex buffer w/empty data!");
-  glBufferSubData(target,
-                  0,
-                  data.size() * sizeof(data[0]),
-                  &data[0]);
-}
--- src/ui/GlUtil.h
+++ src/ui/GlUtil.h
@@ -19,9 +19,20 @@ public:
   /**
    * Creates a vertex buffer, fills buffer w/data, returns name of buffer.
    */
-  static GLuint prepareVertexBuffer(const std::vector<GLfloat>& data,
+  template <typename T>
+  static GLuint prepareVertexBuffer(const std::vector<T>& data,
                                     GLenum target = GL_ARRAY_BUFFER,
-                                    GLenum usage = GL_STATIC_DRAW);
+                                    GLenum usage = GL_STATIC_DRAW)
+  {
+    GLuint buffername = 0;
+    glGenBuffers(1, &buffername);
+    glBindBuffer(target, buffername);
+    glBufferData(target,
+                 data.size() * sizeof(data[0]),
+                 &data[0],
+                 usage);
+    return buffername;
+  }
 
   /**
    * Creates a vertex buffer, does NOT fill buffer w/data
@@ -37,8 +48,17 @@ public:
    * Fills vertex buffer with data.
    * Make sure the buffer is bound to the target array prior to calling.
    */
-  static void fillVertexBuffer(const std::vector<GLfloat>& data,
-                               GLenum target = GL_ARRAY_BUFFER);
+  template <typename T>
+  static void fillVertexBuffer(const std::vector<T>& data,
+                               GLenum target = GL_ARRAY_BUFFER)
+  {
+    assert(!data.empty() && "Cannot fill vertex buffer w/empty data!");
+    glBufferSubData(target,
+                    0,
+                    data.size() * sizeof(data[0]),
+                    &data[0]);
+  }
+
 private:
   GlUtil();
  ~GlUtil();
