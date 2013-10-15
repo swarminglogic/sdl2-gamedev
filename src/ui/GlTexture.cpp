c7f9d34ec7bed04ccd11bc7e817e6532453cbcbf
swarminglogic <github@swarminglogic.com>
2013-10-15 13:50:35 +0200

    Added GlTexture class for binding textures to GL.
    Added GlTexture class for binding textures to GL.
        src/ui/GlTexture.cpp (+)
        src/ui/GlTexture.h   (+)

---
+++ src/ui/GlTexture.cpp
@@ -0,0 +1,56 @@
+#include <ui/GlTexture.h>
+
+#include <cassert>
+
+#include <math/MathUtil.h>
+#include <ui/GlState.h>
+#include <ui/SDL_image.h>
+#include <util/Asset.h>
+
+
+GlTexture::GlTexture()
+  : log_("GlTexture"),
+    textureId_(0),
+    isMaxFiltering_(true)
+{
+}
+
+
+GlTexture::~GlTexture()
+{
+  releaseResources();
+}
+
+
+void GlTexture::releaseResources()
+{
+  if (textureId_) {
+    glDeleteTextures(1, &textureId_);
+    textureId_ = 0;
+  }
+}
+
+void GlTexture::setId(GLuint textureId)
+{
+  releaseResources();
+  textureId_ = textureId;
+}
+
+
+GLuint GlTexture::glBind()
+{
+  assert(textureId_ && "OpenGL texture id not set!");
+  GlState::bindTexture(GL_TEXTURE_2D, textureId_);
+  return textureId_;
+}
+
+
+GLuint GlTexture::getTextureId() const
+{return textureId_;}
+
+
+bool GlTexture::isMaxFiltering() const
+{return isMaxFiltering_;}
+
+void GlTexture::setIsMaxFiltering(bool isMaxFilteringSet)
+{isMaxFiltering_ = isMaxFilteringSet;}
---
+++ src/ui/GlTexture.h
@@ -0,0 +1,53 @@
+#ifndef UI_GLTEXTURE_H
+#define UI_GLTEXTURE_H
+
+#include <string>
+
+#include <math/Rect.h>
+#include <ui/SDL.h>
+#include <ui/SDL_opengl.h>
+#include <util/Log.h>
+
+
+/**
+ * GlTexture class, represents a SDL_GlTexture, and provides
+ * helper functions to interact with OpenGL
+ *
+ * @author SwarmingLogic (Roald Fernandez)
+ */
+class GlTexture
+{
+public:
+  GlTexture();
+
+  virtual ~GlTexture();
+
+  // void setGlTextureId(GLuint textureId, Size size);
+  void setId(GLuint textureId);
+
+  GLuint glBind();
+  void prepareForGl();
+
+  GLuint getTextureId() const;
+
+  /**
+   * If set true, it enables linear mipmapping and anisotropic filtering.
+   * If set false, sets nearest filter, with clamp to edge wrapping.
+   */
+  void setIsMaxFiltering(bool isMaxFiltering);
+  bool isMaxFiltering() const;
+
+private:
+  void releaseResources();
+
+  Log log_;
+  GLuint textureId_;
+  // Rect imageRect_;
+  bool isMaxFiltering_;
+
+  // NonCopyable
+  GlTexture(const GlTexture& c);
+  GlTexture& operator=(const GlTexture& c);
+};
+
+#endif
